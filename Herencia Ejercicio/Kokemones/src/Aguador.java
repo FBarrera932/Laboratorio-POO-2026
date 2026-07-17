bbbf19520952269082f96b6c8c06fbd3cf7228f4
FBarrera932 <barrerafranciscoroman@gmail.com>
2026-07-17 08:08:38 -0300

    Ejercicio 1
    Primer ejercicio de Herencia con pokemones

---
+++ Herencia Ejercicio/Kokemones/src/Aguador.java
@@ -0,0 +1,15 @@
+
+public class Aguador extends Pokemon {
+
+		
+		public Aguador(float vida, int def, int nivel) {
+			super(vida, nivel, def);
+		}
+		
+	
+		
+	public void nadar() {
+		System.out.println("Nadar");
+	}
+		
+}
---
+++ Herencia Ejercicio/Kokemones/src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+	public static void main(String[] args) {
+	 Tierra Geodude = new Tierra(1000, 50, 20); 
+	 Volante Articuno = new Volante(999, 67, 89);
+	 	Geodude.evolucionar();
+	 	Articuno.vuela();
+	}
+
+}
